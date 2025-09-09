b30848cb4896608ccb5b28ffa4904f43000b782d
Daniel Bograd <advantagist@gmail.com>
2025-09-09 19:24:02 +0200

    Adding Dark Mode, Multiple Choice Select,Some bugs fixed,Layout Improved

--- src/script.js
+++ src/script.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   //------- Custom Input Text  Script-------------------------------------------
 
+  const bgSwitch = document.querySelector(".bg-switch");
+  const section = document.querySelector(".sectionContainer");
+  bgSwitch.addEventListener("click", () => {
+    if (section.classList.contains("sectionWhiteTheme")) {
+      section.classList.replace("sectionWhiteTheme", "sectionGrayTheme");
+      bgSwitch.innerText = "White background";
+    } else {
+      section.classList.replace("sectionGrayTheme", "sectionWhiteTheme");
+      bgSwitch.innerText = "Gray background";
+    }
+  });
   const inputReset = document.querySelectorAll(".inpResetBtn");
   inputReset.forEach((el) => {
     el.addEventListener("click", (e) => {
@@ -16,10 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll(".custom-select").forEach((customSelect) => {
     const selected = customSelect.querySelector(".selected");
+    const isMultiChoice = customSelect
+      ? customSelect.classList.contains("custom-select--multi")
+      : false;
     const optionsContainer = customSelect.querySelector(".options-container");
     const searchInput = customSelect.querySelector(".search-input");
     let options = customSelect.querySelectorAll(".option");
     const selectReset = document.querySelector(".inpResetBtn--select");
+    const selectedOptions = [];
 
     let currentIndex = -1;
 
@@ -33,23 +48,119 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-    // Select option
-    const bindOptionClick = () => {
-      options.forEach((option) => {
-        option.addEventListener("click", (e) => {
-          selected.textContent = option.textContent;
-          selected.dataset.value = option.dataset.value;
-          searchInput.value = option.textContent;
-          e.stopPropagation();
-          customSelect.classList.remove("open");
+    //Create a tag
+    function createTag(contentValue) {
+      const container = document.createElement("div");
+      const content = document.createElement("div");
+      const tagRemoveButton = document.createElement("div");
+      const resetButton = document.createElement("div");
 
-          filterOptions("");
-          currentIndex = -1;
+      container.classList.add("tag", "tagSelected");
+      content.classList.add("tagContent");
+      content.innerText = contentValue;
+      tagRemoveButton.classList.add("inp-buttons--tag");
+      tagRemoveButton.setAttribute("tabindex", "0");
+      resetButton.classList.add("inpResetBtn");
+      resetButton.innerHTML = `
+      <svg
+          class="deleteCross"
+          viewBox="0 0 8 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M7 1.90137L4.90137 4L7 6.09863L6.09863 7L4 4.90137L1.90137 7L1 6.09863L3.09863 4L1 1.90137L1.90137 1L4 3.09863L6.09863 1L7 1.90137Z" />
+        </svg>
+      `;
+
+      tagRemoveButton.addEventListener("click", function () {
+        options.forEach((option) => {
+          const control = option.querySelector(".CheckRadioContainer");
+          if (control.innerText === content.innerText) {
+            updateSelectedOptions("remove", content.innerText);
+            control.firstElementChild.checked = false;
+          }
         });
+        this.parentNode.remove();
       });
+
+      tagRemoveButton.appendChild(resetButton);
+      container.append(content, tagRemoveButton);
+
+      return container;
+    }
+
+    // Select option
+    const bindOptionClick = (isMultiChoice) => {
+      if (!isMultiChoice) {
+        options.forEach((option) => {
+          option.addEventListener("click", (e) => {
+            selected.innerText = option.innerText;
+            selected.dataset.value = option.dataset.value;
+            searchInput.value = option.innerText;
+            e.stopPropagation();
+            customSelect.classList.remove("open");
+
+            filterOptions("");
+            currentIndex = -1;
+          });
+        });
+      } else {
+        options.forEach((option) => {
+          option.addEventListener("click", (e) => {
+            let isChecked = option.querySelector(
+              'input[type="checkbox"]',
+            ).checked;
+
+            if (e.target.classList.contains("CheckRadioContainer")) {
+              const optionContent = option.querySelector("label").innerText;
+
+              if (!isChecked && optionContent) {
+                updateSelectedOptions("add", optionContent);
+              } else {
+                updateSelectedOptions("remove", optionContent);
+              }
+
+              redrawTags();
+
+              e.stopPropagation();
+            }
+          });
+        });
+      }
     };
 
-    bindOptionClick();
+    bindOptionClick(isMultiChoice);
+
+    //Updated Selected Options in Multiple Choice Select
+    function updateSelectedOptions(action, element) {
+      if (action === "remove") {
+        const index = selectedOptions.indexOf(element);
+        if (index !== -1) {
+          selectedOptions.splice(index, 1);
+        }
+      } else if (action === "add") {
+        selectedOptions.push(element);
+        selectedOptions.sort();
+      }
+    }
+
+    //Redraw Tags in Multiple Choice Select Alphabetically
+
+    function redrawTags() {
+      if (selected) {
+        // Remove existing tags
+        selected.querySelectorAll(".tag").forEach((el) => {
+          selected.removeChild(el);
+        });
+
+        // Add new tags
+        selectedOptions.forEach((option) => {
+          selected.appendChild(createTag(option));
+        });
+      } else {
+        return;
+      }
+    }
 
     //Reset Select
 
@@ -60,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (input) {
         input.value = "";
         selected.dataset.value = "";
-        selected.textContent = "";
+        selected.innerText = "";
         allOptions.forEach((option) => {
           option.style.display = "";
         });
@@ -84,7 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
     function filterOptions(searchTerm) {
       const allOptions = customSelect.querySelectorAll(".option");
       allOptions.forEach((option) => {
-        const match = option.textContent.toLowerCase().includes(searchTerm);
+        const match = option.innerText.toLowerCase().includes(searchTerm);
         option.style.display = match ? "" : "none";
       });
       options = [...customSelect.querySelectorAll(".option")].filter(
@@ -146,14 +257,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  //------- Text Area t-------------------------------------------
+  //------- Text Area -------------------------------------------
 
   document
     .querySelectorAll(".textareaCounterContainer")
     .forEach((textareaCounterContainer) => {
       const customTextarea =
         textareaCounterContainer.querySelector(".custom-textarea");
-      const input = customTextarea.querySelector(".textarea--white--med");
+      const input = customTextarea.querySelector(
+        ".textarea--white--med, .textarea--dark--med ",
+      );
 
       const resetButton = customTextarea.querySelector(
         ".inp-buttons--textArea",
@@ -176,10 +289,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       resetButton.addEventListener("click", () => {
         textArea.value = "";
+        countCharacters();
         shiftContent();
       });
 
-      //Shifting Reset button and content if there's no
+      //Shifting Reset button and content if there's no scrollbar
       function shiftContent() {
         if (!hasScroll() || !textArea.value) {
           resetButton.style.right = 0;
@@ -194,7 +308,9 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!characterCounter) return;
         const currentLength = textArea.value.length;
         characterCounter.firstChild.textContent = `${currentLength} / `;
-        charLimit.textContent = `${maxLength}`;
+        charLimit.innerText = `${maxLength}`;
+        console.log(characterCounter);
+        console.log(charLimit.innerText);
 
         if (currentLength > maxLength - 1) {
           charLimit.classList.add("charLimit");
@@ -202,10 +318,8 @@ document.addEventListener("DOMContentLoaded", () => {
           charLimit.classList.remove("charLimit");
         }
       }
-
       shiftContent();
       countCharacters();
-
       customTextarea.addEventListener("input", shiftContent);
       customTextarea.addEventListener("input", countCharacters);
       window.addEventListener("resize", shiftContent);
